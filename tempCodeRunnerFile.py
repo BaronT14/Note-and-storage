20b6c79d1dc78f588295d830104f6d9ebaaa492c
Your Name <you@example.com>
2024-04-21 16:41:48 +0700

    ThanhBuoi

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -11,9 +11,23 @@ class GUI:
         self.root.title("NOTE AND STORAGE")
         self.style = Style("minty")
         self.listNote = ds.DSNOTE()
+        #create notebook
+        self.my_notebook=ttk.Notebook(self.root,bootstyle='dark')
+        self.my_notebook.pack(pady=20)
+        self.tab1=ttk.Frame(self.my_notebook)
+
+        self.my_label=Label(tab1, text="test 1", font("default",20))
+        self.my_label.pack(pady=20)
+
+        self.my_text=Text(tab1,width=70,height=10)
+        self.my_text.pack(pady=10, padx=10)
+
+        self.my_notebook.add(tab1,text='tab 1')
+
 
         self.tieude_entry = tk.Entry(root, width=50, font=('Times New Roman', 18, 'italic'))
         self.tieude_entry.grid(row=0, column=2, padx=5, pady=5)
+
         self.tieude_label = tk.Label(root, text="Tiêu đề:")
         self.tieude_label.grid(row=0, column=1, padx=50, pady=50)
 
@@ -23,10 +37,10 @@ class GUI:
         self.noidung_label.grid(row=1, column=1, padx=5, pady=5)
 
         self.add_button = tk.Button(root, text="Thêm ghi chú", command=self.themNote)
-        self.add_button.grid(row=5, column=0, columnspan=2, sticky=W ,padx=5, pady=5)
+        self.add_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
         self.note_listbox = tk.Listbox(root)
-        self.note_listbox.grid(row=5, column=2, rowspan=5, sticky=W,padx=50, pady=(50, 50))
+        self.note_listbox.grid(row=0, column=0, rowspan=3, padx=5, pady=(30, 0))
 
     def themNote(self):
         tieude = self.tieude_entry.get()
@@ -42,7 +56,7 @@ class GUI:
 
     
 def main():
-    root = tk.Tk()
+    root = ttk.Tk()
     root.geometry('1080x600')
     app = GUI(root)
     root.mainloop()
